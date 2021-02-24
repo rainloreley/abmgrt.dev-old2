1646b5688df1d6e80053fe15acbebdb366b32b5b
Adrian Baumgart <adrian@abmgrt.dev>
2021-02-24 21:07:04 +0100

    fixed footer on mobile

--- components/Page.js
+++ components/Page.js
@@ -1,12 +1,29 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import styles from '../styles/Page.module.css';
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function Page({ title, children }) {
+	useEffect(() => {
+		console.log('hi');
+		const appHeight = () => {
+			const doc = document.documentElement;
+			doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+		};
+		window.addEventListener('resize', appHeight);
+		appHeight();
+	}, []);
 	return (
-		<div className="dark:bg-dark-primary w-full h-full min-h-screen mx-auto dark:text-white relative">
-			<Navbar title={title} />
-			<div className="pb-16">{children}</div>
-			<Footer />
-		</div>
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<div className={styles.frame}>
+				<Navbar title={title} />
+				<div className="pb-16">{children}</div>
+				<Footer />
+			</div>
+		</>
 	);
 }
